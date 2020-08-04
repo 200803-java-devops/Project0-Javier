2e1fcec1b95291e1b15b05056969c2277d96251e
jvera-dev <verarj4263@hotmail.com>
2020-08-04 15:30:55 -0500

    commit

---
+++ Project0-Jav/src/main/java/one/App.java
@@ -0,0 +1,13 @@
+package one;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        int a = 1, b = 4;
+
+        a += b;
+
+        System.out.println( "Hello World!" + a );
+    }
+}
