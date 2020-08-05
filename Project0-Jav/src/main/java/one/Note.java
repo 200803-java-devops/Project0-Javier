cb40fd3c6a0cd48f78d7d12c713e5a2d4bb00e5d
jvera-dev <verarj4263@hotmail.com>
2020-08-05 14:13:08 -0500

    Making note class

---
+++ Project0-Jav/src/main/java/one/Note.java
@@ -0,0 +1,43 @@
+package one;
+
+public class Note {
+    private String header;
+    private String body;
+    private String[] tags;
+
+    public String getHeader()
+    {
+        return this.header;
+    }
+
+    public String getBody()
+    {
+        return this.body;
+    }
+
+    public void getTags()
+    {
+        for (int i=0; i<this.tags.length; i++)
+        {
+            System.out.println(tags[i]);
+        }
+    }
+
+    public void setHader()
+    {
+        System.out.println("Please enter Title: ");
+        //need to get input here
+    }
+
+    public void setBody()
+    {
+        System.out.println("Please enter Note: ");
+        //need to get input here
+    }
+
+    public void setTags()
+    {
+        System.out.println("Please enter Tags: ");
+        //need to get input here
+    }
+}
