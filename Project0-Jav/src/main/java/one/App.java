f3a04a01dc84b40b382906a5f8304975365ad4d7
jvera-dev <verarj4263@hotmail.com>
2020-08-07 15:08:25 -0500

    comment notes

--- Project0-Jav/src/main/java/one/App.java
+++ Project0-Jav/src/main/java/one/App.java
@@ -1,10 +1,28 @@
 package one;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class App {
     public static void main(String[] args) {
 
         Note note = new Note();
-        note.allOut();
+        List<Note> noteList = new ArrayList<>();
+        int menuChoise = 0;
+        boolean exit = false;
+        
+        //main menu loop
+        //do{
+        
+        //1: make a new note
+        //2: read an existing note
+        //  2a: manipulate tags
+        //  2b: manipulate body
+        //  2c: manipulate header
+        //3: exit
+
+
+        //} while (!exit)
 
     }
 }
